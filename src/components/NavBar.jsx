58b2a5fa00a61ff06dfa6e8741a6ab95f96d5015
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-10-18 12:58:27 +0200

    popover added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Popover from './popover';
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,11 +20,12 @@ const NavBar = () => {
           </span>
         </a>
         <div className='flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
-          <button
+          <Popover />
+          {/* <button
             type='button'
             className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
             ¿Necesitas ayuda?
-          </button>
+          </button> */}
           <button
             data-collapse-toggle='navbar-sticky'
             type='button'
