172a1c76046d56de07ee9093fa85e182cfc78c1c
Helena <hgonzalezg18@gmail.com>
2024-10-21 14:03:31 +0200

    a tag configured

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { FaPhone } from 'react-icons/fa6';
 
 const Popover = () => {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const popoverRef = useRef(null);
+
+  const handlePopover = () => {
+    setIsPopoverOpen(!isPopoverOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popoverRef.current && !popoverRef.current.contains(event.target)) {
+        setIsPopoverOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
       <button
-        onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+        onClick={handlePopover}
         type='button'
         className='text-white me-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
       >
@@ -14,16 +35,40 @@ const Popover = () => {
       </button>
       {isPopoverOpen && (
         <div
+          ref={popoverRef}
           role='tooltip'
           className='absolute right-10 z-30 mt-12 w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-100 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800'
         >
-          <div className='px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700'>
+          <div className='flex justify-between px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700'>
             <h3 className='font-semibold text-gray-900 dark:text-white'>
-              Popover left
+              Contacto
             </h3>
+            <button type='button' onClick={handlePopover}>
+              <MdClose />
+            </button>
           </div>
-          <div className='px-3 py-2'>
-            <p>And here's some amazing content. It's very engaging. Right?</p>
+          <div className='px-3 py-2 gap-2'>
+            <a
+              href='tel:954435738'
+              className='flex gap-4 border rounded-sm p-2 text-gray-900 my-2'
+            >
+              <FaPhone className='mt-1' />
+              954435738
+            </a>
+            <a
+              href='tel:954435738'
+              className='flex gap-4 border rounded-sm p-2 text-gray-900 my-2'
+            >
+              <FaPhone className='mt-1' />
+              954435738
+            </a>
+            <a
+              href='tel:954435738'
+              className='flex gap-4 border rounded-sm p-2 text-gray-900 my-2'
+            >
+              <FaPhone className='mt-1' />
+              954435738
+            </a>
           </div>
           <div data-popper-arrow></div>
         </div>
