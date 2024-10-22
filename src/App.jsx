3549e1f0a0ed745a89f17062d958b99aae6db42a
Helena <hgonzalezg18@gmail.com>
2024-10-22 17:33:47 +0200

    whatsapp integrated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import NavBar from './components/NavBar';
 import WatchStore from './pages/WatchStore';
 import Footer from './components/Footer';
+import WhatsAppPopup from './components/WhatsAppPopup';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/relojes' element={<WatchStore />} />
       </Routes>
+      <WhatsAppPopup />
       <Footer />
       <Outlet />
     </>
