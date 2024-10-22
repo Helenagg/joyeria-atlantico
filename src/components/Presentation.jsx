7034c119f5e274f63f6fc846aa206bf8bd201262
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-10-22 11:01:02 +0200

    presentation text

---
+++ src/components/Presentation.jsx
@@ -0,0 +1,27 @@
+const Presentation = () => {
+  return (
+    <div className='justify-items-center text-center mt-10'>
+      <h2 className='text-3xl'>Joyería Atlántico desde 1989</h2>
+      <div className='text-left mt-6 mx-36 leading-loose'>
+      <p >
+        Somos la segunda generación de comerciantes de joyería y relojería al
+        por menor, que desempeñan su actividad en Sevilla desde 1989, dando
+        asesoramiento y trato personalizado, trabajando con los mejores
+        proveedores y fabricantes de manera nacional e internacional.
+      </p>
+      <p>
+        De lo mejor de la naturaleza y de las manos de un artista, nace una
+        joya. Clásica, moderna o intemporal, muchas veces capaz de expresar sin
+        palabras, aquello que queremos decir.
+      </p>
+      <p>
+        El tiempo pasa, pero nada mejor que verlo pasar con un bonito reloj en
+        la muñeca, mas que una máquina de medir el tiempo, es el tiempo que
+        puedes disfrutarlo cuando lo llevas
+      </p>
+      </div>
+    </div>
+  );
+};
+
+export default Presentation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Cover from "../components/Cover"
 import HomeGallery from "../components/HomeGallery"
+import Presentation from "../components/Presentation"
 
 const Home = () => {
   return (
     <div className="mt-20 mx-6">
         <HomeGallery />
+        <Presentation />
     </div>
   )
 }
