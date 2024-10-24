3aa9f6b119aadf517725a5a278ee83ecaf32c91a
Helena <hgonzalezg18@gmail.com>
2024-10-24 16:38:06 +0200

    footer accesibility added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import { FaFacebookSquare } from 'react-icons/fa';
 import { FaSquareXTwitter } from 'react-icons/fa6';
 import { AiFillTikTok } from 'react-icons/ai';
 import { FaLinkedin } from "react-icons/fa6";
+import AccessibleLink from './Accessibility/AccessibleLink';
 
 const Footer = () => {
   return (
@@ -9,40 +10,44 @@ const Footer = () => {
       <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
         <div className='md:flex md:justify-between'>
           <div className='mb-6 md:mb-0'>
-            <a href='/' className='flex items-center'>
+            <AccessibleLink href='/' className='flex items-center'>
               <img
                src='/assets/img/logos/logo-joyeria-atlantico.jpeg'
                className='h-14'
-               alt='Joyería Atlántico Logo'
+               alt='Logo de Joyería Atlántico'
               />
-            </a>
+            </AccessibleLink>
             <div className='flex mt-10 sm:justify-center'>
-              <a
+              <AccessibleLink
                 href='#'
                 className='text-secondary hover:text-gray-900 dark:hover:text-white'
+                aria-label='Facebook de Joyería Atlántico'
               >
                 <FaFacebookSquare size={30} />
-              </a>
+              </AccessibleLink>
 
-              <a
+              <AccessibleLink
                 href='#'
                 className='text-secondary hover:text-gray-900 dark:hover:text-white ms-5'
+                aria-label='Twitter de Joyería Atlántico'
               >
                 <FaSquareXTwitter size={30}/>
-              </a>
+              </AccessibleLink>
 
-              <a
+              <AccessibleLink
                 href='#'
                 className='text-secondary hover:text-gray-900 dark:hover:text-white ms-5'
+                aria-label='TikTok de Joyería Atlántico'
               >
                 <AiFillTikTok size={30}/>
-              </a>
-              <a
+              </AccessibleLink>
+              <AccessibleLink
                 href='#'
                 className='text-secondary hover:text-gray-900 dark:hover:text-white ms-5'
+                aria-label='Linkedin de Joyería Atlántico'
               >
                 <FaLinkedin size={30}/>
-              </a>
+              </AccessibleLink>
             </div>
           </div>
 
@@ -50,67 +55,72 @@ const Footer = () => {
             <div>
               <ul className='text-secondary dark:text-gray-400 font-medium'>
                 <li className='mb-4'>
-                  <a href='https://flowbite.com/' className='hover:underline'>
-                    Home
-                  </a>
+                  <AccessibleLink href='/' className='hover:underline'>
+                    Inicio
+                  </AccessibleLink>
                 </li>
                 <li className='mb-4'>
-                  <a
-                    href='https://tailwindcss.com/'
+                  <AccessibleLink
+                    href='/quienes-somos'
                     className='hover:underline'
                   >
                     Quiénes somos
-                  </a>
+                  </AccessibleLink>
                 </li>
                 <li className='mb-4'>
-                  <a
+                  <AccessibleLink
                     href='/contact'
                     className='hover:underline'
                   >
                     Localízame
-                  </a>
+                  </AccessibleLink>
                 </li>
               </ul>
             </div>
             <div>
               <ul className='text-secondary dark:text-gray-400 font-medium'>
                 <li className='mb-4'>
-                  <a
-                    href='https://github.com/themesberg/flowbite'
+                  <AccessibleLink
+                    href='/joyeria'
                     className='hover:underline '
                   >
                     Joyería
-                  </a>
+                  </AccessibleLink>
                 </li>
                 <li className='mb-4'>
-                  <a
-                    href='https://discord.gg/4eeurUVvTy'
+                  <AccessibleLink
+                    href='/relojes'
                     className='hover:underline'
                   >
                     Relojería
-                  </a>
+                  </AccessibleLink>
                 </li>
                 <li className='mb-4'>
-                  <a
-                    href='https://discord.gg/4eeurUVvTy'
+                  <AccessibleLink
+                    href='reseñas'
                     className='hover:underline'
                   >
                     Reseñas
-                  </a>
+                  </AccessibleLink>
                 </li>
               </ul>
             </div>
             <div>
               <ul className='text-secondary dark:text-gray-400 font-medium'>
                 <li className='mb-4'>
-                  <a href='#' className='hover:underline'>
+                  <AccessibleLink href='/privacidad' className='hover:underline'>
                     Política de privacidad
-                  </a>
+                  </AccessibleLink>
+                </li>
+                <li className='mb-4'>
+                  <AccessibleLink href='/legal' className='hover:underline'>
+                    Aviso Legal
+                  </AccessibleLink>
                 </li>
                 <li>
-                  <a href='#' className='hover:underline'>
-                    Aviso Legal
-                  </a>
+                  <AccessibleLink href='/accesibilidad' className='hover:underline'>
+                    Declaración de accesibilidad
+                  </AccessibleLink>
                 </li>
               </ul>
             </div>
