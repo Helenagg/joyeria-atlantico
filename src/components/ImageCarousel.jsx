8307547f713eba11db2e68ac1f57f858aa236c72
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-10-30 11:51:39 +0100

    imageCarousel modified

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -8,25 +8,17 @@ const ImageCarousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const imagesToShow = 4;
 
-  const visibleImages = images.slice(currentIndex, currentIndex + imagesToShow);
-
   const handleNext = () => {
     if (currentIndex < images.length - imagesToShow) {
       setCurrentIndex(currentIndex + 1);
     }
   };
 
-  console.log('New images.len', Math.min(images.length, imagesToShow +1) * (100 / imagesToShow));
-  console.log('images', images.length * (100 / imagesToShow));
-
   const handlePrev = () => {
     if (currentIndex > 0) {
       setCurrentIndex(currentIndex - 1);
     }
   };
-
-  console.log('current', currentIndex);
-  
 
   return (
     <>
@@ -39,51 +31,43 @@ const ImageCarousel = ({ images }) => {
           />
         ))}
       </div>
-      <div className='hidden md:block'>
-        <div className='w-full overflow-hidden'>
-          <div className='flex items-center'>
-            <AccessibleButton
-              type='button'
-              ariaLabel='Ir a imagen previa'
-              className='px-4 py-2 m-8'
-              onClick={handlePrev}
-            >
-              <span className='inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none'>
-                <IoIosArrowDropleft size={40} className='text-primary' />
-              </span>
-            </AccessibleButton>
-            <div className='overflow-hidden w-full'>
+
+      <div className='relative'>
+        <div className='flex overflow-hidden'>
+          {images
+            .slice(currentIndex, currentIndex + imagesToShow)
+            .map((img) => (
               <div
-                className='flex transition-transform duration-500'
-                style={{
-                  transform: `translateX(-${
-                    currentIndex * (80 / imagesToShow)
-                  }%)`,
-                  width: `${(visibleImages.length * 100) / imagesToShow}%`,
-                  // width: `${Math.min(images.length, imagesToShow + 1) * (100 / imagesToShow)}%`
-                }}
-              >
-                {images.map((img) => (
-                  <CardImage
-                    key={img.id}
-                    src={`${urlBase}${img.brand}/${img.src}`}
-                    alt={img.id}
-                  />
-                ))}
+                key={img.id}
+                className='w-1/4 flex transition-transform duration-500'>
+                <CardImage
+                  key={img.id}
+                  src={`${urlBase}${img.brand}/${img.src}`}
+                  alt={img.id}
+                />
               </div>
-            </div>
-            <AccessibleButton
-              type='button'
-              ariaLabel='Ir a imagen siguiente'
-              className='px-4 py-2 m-8'
-              onClick={handleNext}
-            >
-              <span className='inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none'>
-                <IoIosArrowDropright size={40} className='text-primary' />
-              </span>
-            </AccessibleButton>
-          </div>
+            ))}
         </div>
+        <AccessibleButton
+          type='button'
+          ariaLabel='Ir a imagen previa'
+          className='absolute -left-10 top-1/2 transform -translate-y-1/2 z-10'
+          onClick={handlePrev}
+          disabled={currentIndex === 0}>
+          <span className='inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none'>
+            <IoIosArrowDropleft size={40} className='text-primary' />
+          </span>
+        </AccessibleButton>
+        <AccessibleButton
+          type='button'
+          ariaLabel='Ir a imagen siguiente'
+          className='absolute -right-10 top-1/2 transform -translate-y-1/2 z-10'
+          onClick={handleNext}
+          disabled={currentIndex >= images.length - imagesToShow}>
+          <span className='inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none'>
+            <IoIosArrowDropright size={40} className='text-primary' />
+          </span>
+        </AccessibleButton>
       </div>
     </>
   );
