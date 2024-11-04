ec14103ad6753c43e259530526ae5d3197f8c376
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-11-04 11:19:23 +0100

    es locale added

--- src/locale/es.js
+++ src/locale/es.js
@@ -3,3 +3,15 @@ export const sendEmailText = {
   ERROR_MESSAGE_SERVER: 'Error en el servidor, inténtelo de nuevo más tarde',
   SUCCESS_MESSAGE: 'Correo enviado con éxito',
 };
+
+export const contactText = {
+  TITLE: 'Contacta con nosotros',
+  PARAGRAPH:
+    ' Si tienes alguna duda o consulta, así como algún modelo específico no dudas en contactar con nosotros:',
+  EMAIL: 'Correo electrónico',
+  RRSS: 'Redes Sociales',
+  NAME: 'Nombre',
+  SUBJECT: 'Asunto',
+  MESSAGE: 'Mensaje',
+  SUBMIT: 'Enviar',
+};
